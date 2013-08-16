25a3cc44edfc3f8e0d8cdd210d5d25e756304ae0
Jay Kanakiya <kanakiyajay@yahoo.com>
2013-08-16 23:19:33 +0530

    Width & Offset & Add col Working

--- js/app.js
+++ js/app.js
@@ -22,9 +22,10 @@ app.controller('gridCtrl',function  ($scope) {
       }]
   }];
 
-  $scope.inEditor = [];
   $scope.currentRow = 1;
   $scope.currentCol = 1;
+
+  $scope.inEditor = [];
 
   /**
   * Functions
@@ -62,7 +63,11 @@ app.controller('gridCtrl',function  ($scope) {
 
   }
 
-  $scope.addrow = function  () {
+  /**
+  * New Rows and Cols
+  **/
+
+  $scope.addRow = function  () {
     var newrow = {
       row:[{
         md : 1
@@ -71,8 +76,66 @@ app.controller('gridCtrl',function  ($scope) {
     $scope.model.push(newrow);
   }
 
+  $scope.addCol = function  () {
+      var newcol = {
+        md : 1
+      };
+
+      $scope.model[$scope.currentRow].row.push(newcol);
+  }
+  /**
+  * widths and Offsets
+  **/
+
   $scope.increaseWidth = function  () {
-    console.log($scope.getModelData());
+    //Should not be greater than 12
+
+    if($scope.getModelData().md !== 12 )
+      {
+       $scope.getModelData().md += 1 ;
+      }
+    }
+
+    $scope.decreaseWidth = function  () {
+    //Delele if less than 1
+      if($scope.getModelData().md === 1 )
+      {
+       delete $scope.getModelData().md;
+      }
+      else{
+       $scope.getModelData().md -= 1;
+      }
+    }
+
+    $scope.increaseOffset = function  () {
+    //Should not be greater than 10
+    console.log($scope.getModelData().hasOwnProperty('md_offset'));
+
+    if (!$scope.getModelData().hasOwnProperty('md_offset')) {
+      $scope.getModelData().md_offset = 1;
+    }
+    else{
+      if($scope.getModelData().md_offset !== 10 )
+      {
+       $scope.getModelData().md_offset += 1 ;
+      }
+    }
+    }
+
+    $scope.decreaseOffset = function  () {
+    //Delete if less than 1
+    console.log($scope.getModelData().hasOwnProperty('md_offset'));
+    if ($scope.getModelData().hasOwnProperty('md_offset')) {
+
+      if($scope.getModelData().md_offset === 1 )
+      {
+       delete $scope.getModelData().md_offset;
+      }
+      else{
+       $scope.getModelData().md_offset -= 1;
+      }
+    };
+
   }
 
 
